b36228d452ade696419a959b273d5fcdbe216220
AndreGallini <andrea.gallini01@libero.it>
2022-12-12 12:31:31 +0100

    add schaffholding

---
+++ Models/Movies.php
@@ -0,0 +1,18 @@
+<?php
+class Movie
+{
+    public $titolo;
+    public $descrizione;
+    public $uscita;
+    public $genere;
+
+
+
+    public function __construct($_titolo, $_descrizione, $_uscita, $_genere)
+    {
+
+    }
+}
+
+
+?>
