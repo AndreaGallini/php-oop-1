cbd2b0bf1432b49bbb70a66947823e1818b60219
AndreGallini <andrea.gallini01@libero.it>
2022-12-12 12:59:22 +0100

    fix

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 include __DIR__ . '/Models/Movies.php';
 $avatar = new Movie('Avatar', 'un bel film', '2022', 'fantasy');
-var_dump($avatar)
 
-    ?>
+echo 'Titolo :' . ' ' . $avatar->titolo;
+echo ' <br> Descrizione del film :' . ' ' . $avatar->descrizione;
+echo '<br>Anno di Pubblicazione :' . ' ' . $avatar->uscita;
+echo '<br>Genere :' . ' ' . $avatar->genere;
+?>
